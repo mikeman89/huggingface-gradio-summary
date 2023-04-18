d3b278f8c8c6bb4fcd9eec16813a3f7ee855bf2c
Michael Al Tork <michael.altork@gmail.com>
2023-04-17 23:14:43 -0400

    added model comments

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import gradio as gr
 
 from article import extract_article, get_config
 
-USER_AGENT = """
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36
-"""
+USER_AGENT = """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"""
 
 DESC = """
         Let Hugging Face models summarize articles for you. 
@@ -32,6 +30,9 @@ def extract_article_partial(url: str):
 
 extractor = gr.Interface(extract_article_partial, "text", "text")
 summarizer = gr.load("huggingface/facebook/bart-large-cnn")
+
+# mrm8488/camembert2camembert_shared-finetuned-french-summarization
+# facebook/bart-large-cnn
 
 
 iface = gr.Series(
--- article.py
+++ article.py
@@ -8,7 +8,13 @@ def get_config(user_agent: str) -> Config:
     return config
 
 
+def remove_whitespace(url: str) -> str:
+    url = url.strip()
+    return url
+
+
 def extract_article(config: Config, url: str) -> str:
+    url = remove_whitespace(url)
     article = Article(url, config=config)
     article.download()
     article.parse()
